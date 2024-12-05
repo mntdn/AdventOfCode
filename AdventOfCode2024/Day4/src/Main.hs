c6167cc6054bca7856e0055571ce69ea4b31a4f5
Matthieu <matmon@gmail.com>
2024-12-05 09:26:38 +0700

    Day 4 1 OK

--- AdventOfCode2024/Day4/src/Main.hs
+++ AdventOfCode2024/Day4/src/Main.hs
@@ -18,8 +18,17 @@ getDiag s a b isRight
   | not isRight && a >= 0 && b < length s = ((s!!b)!!a):getDiag s (a-1) (b+1) isRight
   | otherwise = []
 
--- getAllDiag :: [String] -> [String]
-getAllDiag s = foldl (++) [] [[getDiag s x 0 t | x <- [0..(length (s!!0) - 1)]] ++ [getDiag s 0 x t | x <- [1..(length s - 1)]] | t <- [True, False]]
+getAllDiag :: [String] -> [String]
+getAllDiag s = [getDiag s x 0 True | x <- [0..(length (s!!0) - 1)]] ++ [getDiag s 0 x True | x <- [1..(length s - 1)]] 
+  ++ [getDiag s x 0 False | x <- reverse [0..(length (s!!0) - 1)]] ++ [getDiag s (length (s!!0) - 1) x False | x <- reverse [1..(length s - 1)]]
+
+getVert :: [String] -> Int -> Int -> String
+getVert s a b 
+  | b < length s = ((s!!b)!!a):getVert s a (b+1)
+  | otherwise = []
+
+getAllVert :: [String] -> [String]
+getAllVert s = [getVert s x 0 | x <- [0..(length (s!!0) - 1)]]
 
 testXmas :: String -> Int
 testXmas s = sum [length (getAllTextMatches (t =~ "XMAS") :: [String]) | t <- [s, reverse s]]
@@ -29,7 +38,8 @@ main = do
   let list = []
   handle <- openFile "./data.txt" ReadMode
   contents <- getlines handle
-  putStrLn (show (getAllDiag contents))
-  putStrLn (show ([testXmas x | x <- getAllDiag contents]))
-  putStrLn (show (sum [testXmas x | x <- contents] + sum [testXmas x | x <- getAllDiag contents]))
+  -- putStrLn (show (getAllDiag contents))
+  -- putStrLn (show ([testXmas x | x <- getAllDiag contents]))
+  -- putStrLn (show (sum [testXmas x | x <- contents] + sum [testXmas x | x <- getAllDiag contents]))
+  putStrLn (show (sum [sum (map (\a -> testXmas a) x) | x <- [contents, getAllDiag contents, getAllVert contents]]))
   hClose handle   
